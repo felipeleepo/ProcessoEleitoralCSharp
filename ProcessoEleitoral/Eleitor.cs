3f789b958cff39e036ad03bbf6eb9d975eb02e9b
felipeleepo <felipebnf@outlook.com>
2017-02-17 17:52:33 -0200

    Add files via upload

---
+++ ProcessoEleitoral/Eleitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessoEleitoral
+{
+    class Eleitor : IControle
+    {
+        private String nome;
+        private int titulo;
+
+        public String TratarDados()
+        {
+            return nome + ";" + titulo + "@";
+        }
+
+        public Eleitor(String nome, int titulo)
+        {
+            this.nome = nome;
+            this.titulo = titulo;
+        }
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public int Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+
+            set
+            {
+                titulo = value;
+            }
+        }
+    }
+}
