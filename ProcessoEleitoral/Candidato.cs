883acb1f036fc321a23ea3a8d373bd6367505167
yuriscosta <yuri.sales@protonmail.com>
2017-02-21 21:08:02 -0300

    altera diagrama de classes e acrescenta classes

---
+++ ProcessoEleitoral/Candidato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessoEleitoral
+{
+    class Candidato : IControle
+    {
+        private int id;
+        private string nome;
+        private int numero;
+        private string partido;
+
+        public Candidato(string nome, int numero, string partido)
+        {
+            this.nome = nome;
+            this.numero = numero;
+            this.partido = partido;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public string Partido
+        {
+            get { return partido; }
+            set { partido = value; }
+        }
+
+        public string TratarDados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ProcessoEleitoral/Eleitor.cs
+++ ProcessoEleitoral/Eleitor.cs
@@ -8,44 +8,55 @@ namespace ProcessoEleitoral
 {
     class Eleitor : IControle
     {
+        private int id;
         private String nome;
         private int titulo;
         private int cpf;
+        private int zona;
+        private int secao;
 
-        public String TratarDados()
-        {
-            return nome + ";" + titulo + "@";
-        }
-
-        public Eleitor(String nome, int titulo)
+        public Eleitor(String nome, int titulo, int cpf, int zona, int secao)
         {
             this.nome = nome;
             this.titulo = titulo;
+            this.cpf = cpf;
+            this.zona = zona;
+            this.secao = secao;
         }
+
         public string Nome
         {
-            get
-            {
-                return nome;
-            }
-
-            set
-            {
-                nome = value;
-            }
+            get { return nome; }
+            set { nome = value; }
         }
 
         public int Titulo
         {
-            get
-            {
-                return titulo;
-            }
+            get { return titulo; }
+            set { titulo = value; }
+        }
 
-            set
-            {
-                titulo = value;
-            }
+        public int Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        public int Zona
+        {
+            get { return zona; }
+            set { zona = value; }
+        }
+
+        public int Secao
+        {
+            get { return secao; }
+            set { secao = value; }
+        }
+
+        public String TratarDados()
+        {
+            return nome + ";" + titulo + "@";
         }
     }
 }
